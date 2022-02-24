20cb83015e679da0a3014740f43f62186e803262
Vasiliy Pankrukhin <wacpan93@gmail.com>
2022-02-24 18:55:57 +0300

    добавление Service Discovery

---
+++ ConsoleApp/ConsulServiceProvider.cs
@@ -0,0 +1,22 @@
+﻿using Consul;
+
+namespace ConsoleApp
+{
+    public class ConsulServiceProvider
+    {
+        public async Task<List<string>> GetServicesAsync()
+        {
+            var consuleClient = new ConsulClient(consulConfig =>
+            {
+                consulConfig.Address = new Uri("http://localhost:8500");
+            });
+            var queryResult = await consuleClient.Health.Service("Service", string.Empty);
+            var result = new List<string>();
+            foreach (var serviceEntry in queryResult.Response)
+            {
+                result.Add($"{serviceEntry.Service.Address}:{serviceEntry.Service.Port}, {serviceEntry.Checks[0].Name}:{serviceEntry.Checks[0].Status.Status}");
+            }
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleApp;
+
+var Client = new ConsulServiceProvider();
+var result = await Client.GetServicesAsync();
+foreach (var service in result)
+{
+    Console.WriteLine(service);
+}
+
